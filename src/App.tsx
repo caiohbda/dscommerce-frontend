2fecafc5cc9c24dfa8e5f85c76a5ed2ecfddf643
Caio <caiohbda777@gmail.com>
2025-01-13 14:58:50 -0300

    definindo rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Catalog } from "./routes/Catalog";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ClientHome } from "./routes/ClientHome";
+import { ProductDetails } from "./routes/ProductDetails";
 
 const App = () => {
-  return <Catalog />;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ClientHome />}></Route>
+      <Route path="product-details" element={<ProductDetails />} />
+    </Routes>
+  </BrowserRouter>;
 };
 
 export default App;
